2af4b592d777ab04b25470631223177434436947
Hikmatulloh-sd <hikmatulloh.sd@gmail.com>
2024-12-22 17:20:10 +0600

    Variables

---
+++ lesson_1.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main(){
+	// Valiable
+
+	// message := "Hello world	"
+	// message := ""
+	// message = "Hello Go"
+
+	var message string 
+	message = "Hello Go"
+
+	const name string = "Hikmatulloh"
+
+	fmt.Println(message)
+	fmt.Println(name)
+}
